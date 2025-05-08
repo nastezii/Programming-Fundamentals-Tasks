ea84479445375e71f5cc9997a3d6494509be2a2c
nastezii <nastiashalak@gmail.com>
2025-05-08 21:34:16 +0300

    implement Node class

---
+++ Task_3/Node.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_3
+{
+    public class Node
+    {
+        public T Item { get; private set; }
+        public int Priority { get; private set; }
+        public long Timestamp { get; private set; }
+
+        public Node(T item, int priority, long timestamp)
+        {
+            Item = item;
+            Priority = priority;
+            Timestamp = timestamp;
+        }
+    }
+}
