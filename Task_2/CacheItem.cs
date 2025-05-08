9fd45c0ceb4f72897dc001320c53513eebdd75d4
nastezii <nastiashalak@gmail.com>
2025-05-08 13:25:02 +0300

    create CacheItem<T> class to store cached result with metadata

---
+++ Task_2/CacheItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_2
+{
+    public class CacheItem<T>
+    {
+        public T Result { get; private set; }
+        public DateTime CreationTime { get; private set; }
+        public DateTime LastAccessed { get; private set; }
+        public int Frequency { get; private set; }
+
+        public CacheItem(T result)
+        {
+            Result = result;
+            LastAccessed = CreationTime = DateTime.Now;
+            Frequency = 1;
+        }
+
+        public void RecordAccess()
+        {
+            LastAccessed = DateTime.Now;
+            Frequency++;
+        }
+    }
+}
