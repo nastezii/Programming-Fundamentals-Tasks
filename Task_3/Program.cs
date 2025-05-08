70cd4e5bb1bf02cf3e6cb97de2e1865ca2283d57
nastezii <nastiashalak@gmail.com>
2025-05-09 01:35:29 +0300

    add example

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using Task_3;
+
+var queue = new BiDirectionalPriorityQueue<string>();
+
+
+queue.Enqueue("apple", 2);
+queue.Enqueue("banana", 3);
+queue.Enqueue("cherry", 1);
+
+
+Console.WriteLine($"Highest priority item: {queue.Peek(Mode.Highest)}"); 
+
+var dequeuedItem = queue.Dequeue(Mode.Highest);
+Console.WriteLine($"Dequeued item: {dequeuedItem}"); 
+
+Console.WriteLine($"Newest item: {queue.Peek(Mode.Newest)}"); 
+
+dequeuedItem = queue.Dequeue(Mode.Newest);
+Console.WriteLine($"Dequeued newest item: {dequeuedItem}"); 
+
+Console.WriteLine($"Oldest item: {queue.Peek(Mode.Oldest)}"); 
+
+dequeuedItem = queue.Dequeue(Mode.Oldest);
+Console.WriteLine($"Dequeued oldest item: {dequeuedItem}"); 
